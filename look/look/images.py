00a8a2481203da28cfe8df0149fb7ce96498d2c3
Adarsh Murthy <am4417@columbia.edu>
2018-08-14 20:30:50 -0400

    Updates the images file to render the resource as message pack.

--- look/look/images.py
+++ look/look/images.py
@@ -1,6 +1,6 @@
-import json
-
 import falcon
+
+import msgpack
 
 
 class Resource(object):
@@ -14,8 +14,8 @@ class Resource(object):
             ]
         }
 
-        # Create a JSON representation of the resource
-        resp.body = json.dumps(doc, ensure_ascii=False)
+        resp.data = msgpack.packb(doc, use_bin_type=True)
+        resp.content_type = falcon.MEDIA_MSGPACK
 
         # The following line can be omitted because 200 is the default
         # status returned by the framework, but it is included here to
